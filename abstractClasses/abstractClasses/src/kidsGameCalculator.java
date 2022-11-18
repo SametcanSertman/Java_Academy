e1fd0654b09fa29d8fe337ec6eea6bebed09046f
SametcanSertman <118531767+SametcanSertman@users.noreply.github.com>
2022-11-18 14:40:55 +0300

    Add files via upload

---
+++ abstractClasses/abstractClasses/src/kidsGameCalculator.java
@@ -0,0 +1,8 @@
+public class kidsGameCalculator {
+    public class KidsGameCalculator extends GameCalculator{
+        @Override
+        public void hesapla() {
+            System.out.println("puanınız : 100");
+
+        }
+    }
